afd2d502822779492cb22f2bea1b358d4cb35a88
nbdigitech <31731388+nbdigitech@users.noreply.github.com>
2025-03-22 15:20:39 +0530

    Update page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -61,7 +61,7 @@ export default function AuthPage() {
     }
   
     try {
-      const response = await fetch("https://cmophotos.in/login", {
+      const response = await fetch("http://167.86.74.16:5000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
